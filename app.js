3db16129b57f2dd6588b613c8b7bcc2445b55379
cloud-official <tixu.ramp@gmail.com>
2025-06-05 11:54:18 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+let deferredPrompt;
+const installBtn = document.getElementById('installBtn');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  deferredPrompt = e;
+  installBtn.style.display = 'inline-block';
+
+  installBtn.addEventListener('click', () => {
+    installBtn.style.display = 'none';
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then(choiceResult => {
+      deferredPrompt = null;
+    });
+  });
+});
